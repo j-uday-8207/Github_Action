90059aa88d443c91c60f2910ebd3efa87c84c72d
j-uday-8207 <udayjain8207@gmail.com>
2023-05-20 22:12:38 +0530

    New

---
+++ FunnyText.js
@@ -0,0 +1,7 @@
+const Funny_Text = ['Knock, knock. Who is  there? Impatience. Impatience who? Sorry, I could not wait to tell you this joke!', 'Just Laugh and Lose Some Extra Calories You Fat Man/Woman. jk jk ',
+'I think this Waiting Period Turns Me On.... Am I The only one? ','Warning: Waiting may lead to excessive doodling and daydreaming. Proceed with caution!',
+'Yayy!! You are just few hundred minutes away to get a degree for waiting. Thank Me Later..','I hope You End Up Waiting Here.. '];
+
+const Index = Math.floor(Math.random()*Funny_Text.length);
+const Message = Funny_Text[Index];
+console.log(Message);
---
+++ readme.md
@@ -0,0 +1,12 @@
+Name  : Uday Jain
+
+
+
+Roll : 220106086
+
+
+Branch : BSBE
+
+
+
+This Is a Github Action Repo... It Just Prints Funny Text (Acoording To Me) during a PR.
